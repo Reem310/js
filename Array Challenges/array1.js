977851d2d2ec6597d12ceb1b04d22952aae11c16
Reem Bm <ryame.3@gmail.com>
2022-05-26 23:22:17 +0300

    git

---
+++ Array Challenges/array1.js
@@ -0,0 +1,16 @@
+function alwaysHungry(arr) {
+    var count = 0;
+    for(var i = 0; i < arr.length; i++) {
+        if(arr[i] == 'food') {
+            count++;
+            console.log('yummy');
+        } 
+    }
+    if(count == 0) {
+        console.log('I\'m hungry');
+    }
+}
+alwaysHungry([3.14, "food", "pie", true, "food"]);
+// this should console log "yummy", "yummy"
+alwaysHungry([4, 1, 5, 7, 2]);
+// this should console log "I'm hungry"
