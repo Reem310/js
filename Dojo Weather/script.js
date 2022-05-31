c0f8b0818c074194c2a7fa2e014c802cf7069592
Reem Bm <ryame.3@gmail.com>
2022-05-31 20:09:29 +0300

    git

--- Dojo Weather/script.js
+++ Dojo Weather/script.js
@@ -5,15 +5,15 @@ function accept() {
 }
 var ht = document.querySelectorAll(".ht");
 var cd = document.querySelectorAll(".cd");
-function change_temp(i){
-    if(i.value=="C"){
-        for(var i =0;i<ht.length;i++){
+function change_temp(e){
+    if(e.value=="C"){
+        for(var i=0;i<ht.length;i++){
             ht[i].innerHTML = Math.round((ht[i].innerHTML-32)*5/9);
             cd[i].innerHTML = Math.round((cd[i].innerHTML-32)*5/9);
         }
     }
-    else if(i.value=="F"){
-    for(var i =0;i<ht.length;i++){
+    else if(e.value=="F"){
+    for(var i=0;i<ht.length;i++){
         ht[i].innerHTML = Math.round((ht[i].innerHTML*9/5)+32);
         cd[i].innerHTML = Math.round((cd[i].innerHTML*9/5)+32);
     }
